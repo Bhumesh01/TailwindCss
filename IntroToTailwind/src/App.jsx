a6e18642a5104b3f7e7818865f6b0eff9311e346
Bhumesh Mahajan <167601736+Bhumesh01@users.noreply.github.com>
2025-06-11 19:01:02 +0530

    Flex box without tailwind

--- IntroToTailwind/src/App.jsx
+++ IntroToTailwind/src/App.jsx
@@ -3,7 +3,18 @@ import './App.css'
 function App() {
   return (
     <>
-    <h1>Welcome</h1>
+    <Test></Test>
+    </>
+  )
+}
+function Test(){
+  return(
+    <>
+    <div style={{display: 'flex', justifyContent: 'space-around'}}>
+      <div style={{padding: '5px', borderRadius: '2px',color: 'yellow',backgroundColor: 'green'}}>Hi there from <b>first</b> div</div>
+      <div style={{padding: '5px', borderRadius: '2px',color: 'white',backgroundColor: 'red'}}>Hi there from <b>second</b> div</div>
+      <div style={{padding: '5px', borderRadius: '2px',color: 'black',backgroundColor: 'pink'}}>Hi there from <b>third</b> div</div>
+    </div>
     </>
   )
 }
