17ee45c595346b4fe5701fa4e05d63b751fdfe38
Bhumesh Mahajan <167601736+Bhumesh01@users.noreply.github.com>
2025-06-11 20:02:31 +0530

    FlexBox using Tailwind

--- IntroToTailwind/src/App.jsx
+++ IntroToTailwind/src/App.jsx
@@ -10,10 +10,15 @@ function App() {
 function Test(){
   return(
     <>
-    <div style={{display: 'flex', justifyContent: 'space-around'}}>
+    {/* <div style={{display: 'flex', justifyContent: 'space-around'}}>
       <div style={{padding: '5px', borderRadius: '2px',color: 'yellow',backgroundColor: 'green'}}>Hi there from <b>first</b> div</div>
       <div style={{padding: '5px', borderRadius: '2px',color: 'white',backgroundColor: 'red'}}>Hi there from <b>second</b> div</div>
       <div style={{padding: '5px', borderRadius: '2px',color: 'black',backgroundColor: 'pink'}}>Hi there from <b>third</b> div</div>
+    </div> */}
+    <div className='flex justify-around'>
+      <div className='p-3 text-yellow-300 bg-green-600 rounded-xl'>Hi there from <b>first</b> div</div>
+      <div className='p-3 text-white bg-red-600 rounded-xl'>Hi there from <b>second</b> div</div>
+      <div className='p-3 text-black bg-pink-200 rounded-xl'>Hi there from <b>third</b> div</div>
     </div>
     </>
   )
