3a9ac8f9bb6d79e50e11aeecf6dfe8881bbdbe0b
Bhumesh Mahajan <167601736+Bhumesh01@users.noreply.github.com>
2025-06-13 19:03:43 +0530

    added transition property

--- TailwindPart2/src/components/1-basic-project.jsx
+++ TailwindPart2/src/components/1-basic-project.jsx
@@ -1,7 +1,7 @@
 function SidebarClass1(){
     return(
         <div className="h-screen flex">
-            <div className="transform-all duration-1000 bg-red-200 hover:bg-yellow-200 p-4 hover:p-8 hover:font-bold sm:flex-1 sm:block hidden duration-300 md:w-[25%]">
+            <div className="transition-all ease-in-out duration-150 bg-red-200 hover:bg-yellow-200 p-4 hover:p-8 hover:font-bold sm:w-36 w-0 duration-300 md:w-96">
                 SideBar
             </div>
             <div className="bg-green-200 flex-4">
