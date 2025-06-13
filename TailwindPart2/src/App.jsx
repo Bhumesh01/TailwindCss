6ad611c1733bfec28978d59a52a0cdf03369b14c
Bhumesh Mahajan <167601736+Bhumesh01@users.noreply.github.com>
2025-06-13 19:27:22 +0530

    Mode toggling automatically

--- TailwindPart2/src/App.jsx
+++ TailwindPart2/src/App.jsx
@@ -2,11 +2,10 @@ import { useState } from 'react'
 import './App.css'
 import SidebarClass1 from './components/1-basic-project'
 function App() {
-  const [darkMode, setdarkMode]= useState(false);
   return (
     <>
-      <div className={`h-screen ${darkMode?'bg-black text-white': 'bg-white text-lack'}`}>
-        <button className={`border ${darkMode?'border-white': 'border-black'} p-2 m-1 rounded-xl`} onClick={()=>setdarkMode(prev=>!prev)}>Change Mode ☀︎</button>
+      <div className='h-screen bg-white text-black dark:bg-black dark:text-white'>
+        <h1>Toggle Theme</h1>
         {/* <SidebarClass1></SidebarClass1> */}
       </div>
     </>
